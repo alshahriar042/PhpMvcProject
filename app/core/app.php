31e1d69843ab917b148cdf7f05d47d37495b047d
AL Shahriar <shariarakash@gmail.com>
2022-02-11 23:27:46 +0600

    first commit

---
+++ app/core/app.php
@@ -0,0 +1,16 @@
+<?php
+
+class App
+{
+    public function __construct()
+    {
+        $url= $this->splitURL();
+         show($url);
+    }
+
+    private function splitURL()
+    {
+         return explode("/" ,trim($_GET['url'],"/")) ;
+    }
+}
+ 
